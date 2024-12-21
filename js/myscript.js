7436e1ba117e195bc9bf97533d8e09f9e21a1572
Ramon Hernandez <75559977+ramonpsx95@users.noreply.github.com>
2024-12-20 21:24:54 -0600

    Add files via upload

---
+++ js/myscript.js
@@ -0,0 +1,18 @@
+function texto(){
+    document.getElementById('test').innerHTML="el texto ha sido cambiado";
+}
+
+
+function mensaje(){
+    alert('este es un mensajito nomas !');
+}
+
+
+function suma()
+{
+    var num1 = 1800;
+    var num2 = 1800;
+    var suma =  num1 + num2;
+
+    document.getElementById('text').innerHTML= suma ;
+}
